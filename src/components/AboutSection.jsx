f3e9ea1ef4a3ebb09391a591e72ec52cba073bd0
Saleh Muhammad <salehsegeir1717@gmail.com>
2025-12-09 16:43:42 +0700

    Remove old CV file

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/cv/Saleh-Muhammad-CV.pdf"
+                href="/cv/My_CV.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
